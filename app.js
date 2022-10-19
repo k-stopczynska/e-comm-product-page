da4383776b9b7e2e3cf0e100c60d5f025f1f0548
Klaudia Stopczynska <k.stopczynska@gmail.com>
2022-10-19 08:37:30 +0200

    Adding cart

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const thumbsImageList = document.querySelector(".wrapper__thumbs");
 const lightbox = document.querySelector(".lightbox");
 const arrows = document.querySelectorAll(".arrow");
 const form = document.querySelector(".wrapper__add__product");
+const wrapperCart = document.querySelector(".wrapper__cart");
+const cart = wrapperCart.querySelector(".cart");
 
 const menuHandler = () => {
   menu.classList.add("active");
@@ -38,6 +40,8 @@ const lightboxHandler = () => {
   closeLightBox.classList.add("button__close__lightbox");
   lightBox.appendChild(closeLightBox);
   document.body.appendChild(lightBox);
+ 
+  closeLightBox.addEventListener("click", closeLightboxHandler);
   const arrows = document.querySelectorAll(".arrow");
   arrows.forEach((arrow) => {
     arrow.addEventListener("click", wrapperImageHandler);
@@ -49,10 +53,10 @@ const lightboxHandler = () => {
 };
 
 const closeLightboxHandler = () => {
-    overlay.classList.remove('active');
-   const lightBox = document.querySelector('.visible');
-   lightBox.remove();
-}
+  overlay.classList.remove("active");
+  const lightBox = document.querySelector(".visible");
+  lightBox.remove();
+};
 
 const thumbImageHandler = (e) => {
   const elements = e.currentTarget.children;
@@ -68,9 +72,69 @@ const thumbImageHandler = (e) => {
   }
 };
 
+const removeCartItems = () => {
+  cart.children[1].style.display = "flex";
+  cart.removeChild(cart.children[2]);
+  cart.removeChild(cart.children[2]);
+  wrapperCart.removeChild(wrapperCart.children[1]);
+};
+
+const checkout = () => {
+  removeCartItems();
+  const thanks = document.createElement("h3");
+  thanks.innerText = "Thank you for your order!";
+  cart.appendChild(thanks);
+};
+
+const calculateCartSum = (price, value) => {
+  return (parseInt(price) * value).toFixed(2);
+};
+
+const createCartThumb = (value) => {
+  const cartThumb = document.createElement("div");
+  cartThumb.classList.add("cart__thumb");
+  cartThumb.innerText = `${value}`;
+  wrapperCart.appendChild(cartThumb);
+};
+
+const createCart = (thumb, title, price, value) => {
+  cart.children[1].style.display = "none";
+  cart.insertAdjacentHTML(
+    "beforeend",
+    `  <div class="wrapper__cart__inside">
+         <div class="wrapper__cart__image">${thumb}</div>
+         <div>
+           <h4>${title}</h4>
+           <p>$${price} x <span>${value} </span><span>$${calculateCartSum(
+      price,
+      value
+    )}</span></p>
+         </div>
+        <button class="button__trash"></button>
+      </div>
+    <button class="button__checkout">Checkout</button>`
+  );
+  createCartThumb(value);
+  const buttonTrash = cart.querySelector(".button__trash");
+  const buttonCheckout = cart.querySelector(".button__checkout");
+  wrapperCart.appendChild(cart);
+  buttonTrash.addEventListener("click", removeCartItems);
+  buttonCheckout.addEventListener("click", checkout);
+};
+
 const addToCartHandler = (e) => {
   e.preventDefault();
-  console.log(e.currentTarget);
+  const thumb = thumbsImageList.children[0].innerHTML;
+  const title = document.querySelector("h1").innerText;
+  const price = e.target.previousElementSibling.children[0].dataset.value;
+  const value = e.target.children[0].children[1].value;
+  if (e.target.children[1]) {
+    createCart(thumb, title, price, value);
+  }
+};
+
+showCartHandler = () => {
+  cart.classList.add("visible");
 };
 
 hamburger.addEventListener("click", menuHandler);
@@ -81,7 +145,10 @@ arrows.forEach((arrow) => {
 form.addEventListener("submit", addToCartHandler);
 mainImageList.addEventListener("click", lightboxHandler);
 thumbsImageList.addEventListener("click", thumbImageHandler);
-overlay.addEventListener('click', closeLightboxHandler);
-if (lightbox.classList.contains('visible')) {
-    lightbox.querySelector('button').addEventListener('click', closeLightboxHandler)
-};
+overlay.addEventListener("click", closeLightboxHandler);
+if (lightbox.classList.contains("visible")) {
+  lightbox
+    .querySelector("button")
+    .addEventListener("click", closeLightboxHandler);
+}
+wrapperCart.addEventListener("click", showCartHandler);
